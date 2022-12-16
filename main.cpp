76b6803b0aa48cd9323c5629c7fe80a2e50b146d
Lilia Ordinyan <101642325+ordinyanlilia@users.noreply.github.com>
2022-12-16 12:38:01 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,22 +9,22 @@ int main() {
 	Shablon obj1(s1);
 	cout << "Shablon" << endl;
 	cout << "Original string is: " << s1 << endl;
-	cout << "Encrypted string is: " << obj1.dCoder() << endl;
-	cout << "Decrypted string is: " << obj1.nCoder() << endl << endl;
+	cout << "Encrypted string is: " << obj1.vercanum() << endl;
+	cout << "Decrypted string is: " << obj1.gaxtnagrum() << endl << endl;
 
 	//Toxer/Syuner
 	string s2 = "abc abc a";
 	ToxerSyuner obj2(s2, 3);
 	cout << "Toxer/Syuner" << endl;
 	cout << "Original string is: " << s2 << endl;
-	cout << "Encrypted string is: " << obj2.dCoder() << endl;
-	cout << "Decrypted string is: " << obj2.nCoder() << endl << endl;
+	cout << "Encrypted string is: " << obj2.vercanum() << endl;
+	cout << "Decrypted string is: " << obj2.gaxtnagrum() << endl << endl;
 
 	//RLE
 	string s3 = "aaaaaaaaaaabbbNNNNNN";
 	RLE obj3(s3);
 	cout << "RLE" << endl;
 	cout << "Original string is: " << s3 << endl;
-	cout << "Encrypted string is: " << obj3.dCoder() << endl;
-	cout << "Decrypted string is: " << obj3.nCoder() << endl << endl;
+	cout << "Encrypted string is: " << obj3.vercanum() << endl;
+	cout << "Decrypted string is: " << obj3.gaxtnagrum() << endl << endl;
 }
